ef8bd9d8eb59ea5c4575f25f14d0b670f8c26e26
NaTheFunny <matiasbetorr@gmail.com>
2024-12-03 22:52:05 -0300

    Vercel

--- src/server.js
+++ src/server.js
@@ -3,12 +3,11 @@ const cors = require('cors');
 const ratingRoutes = require('./routes/ratingRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/ratings', ratingRoutes); 
+app.use('/api/ratings', ratingRoutes);
 
-app.listen(PORT, () => {
-    console.log(`API escuchando en http://localhost:${PORT}`);
-});
+module.exports = (req, res) => {
+  app(req, res);
+};
